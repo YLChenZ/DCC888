7ff1689f529f2606d3de526ae0e83beb707c81d4
YLChen <chentongyongcz@gmail.com>
2025-03-06 22:43:51 +0800

    Translate the program using the given three-address code

--- ControlFlowGraphs/todo.py
+++ ControlFlowGraphs/todo.py
@@ -11,7 +11,7 @@ def test_min(m, n):
         >>> test_min(4, 3)
         3
     """
-    env = Env({"m": m, "n": n, "x": m, "zero": 0})
+    env = Env({"m": m, "n": n,  "zero": 0})
     m_min = Add("answer", "m", "zero")
     n_min = Add("answer", "n", "zero")
     p = Lth("p", "n", "m")
@@ -19,7 +19,7 @@ def test_min(m, n):
     p.add_next(b)
     interp(p, env)
     return env.get("answer")
-
+# print(test_min(3,1100))
 
 def test_fib(n):
     """
@@ -60,9 +60,23 @@ def test_min3(x, y, z):
         >>> test_min3(5, 4, 3)
         3
     """
-    # TODO: Implement this method
+    # env = Env({"cond" : test_min(x,y) == test_min(y,z), "Ans" : test_min(x,z),"Y": y, "zero": 0})
+
+    # b1 = Add("answer","Y","zero")
+    # b2 = Add("answer","Ans","zero")
+    # i0 = Bt("cond",b1,b2)
+    
+    # interp(i0, env)
+    a = test_min(x,y)
+    b = test_min(a,z)
+
+    env = Env({"B" : b, "zero" : 0})
+    i0 = Add("answer","B","zero")
+
+    interp(i0, env)
     return env.get("answer")
 
+print(test_min3(4, 4, 5))
 
 def test_div(m, n):
     """
@@ -76,9 +90,20 @@ def test_div(m, n):
         >>> test_div(1, 3)
         0
     """
-    # TODO: Implement this method
-    return env.get("answer")
+    env = Env({"M" : m, "N" : n, "sum" : n, "count": 0, "zero" : 0,"one" : 1})
 
+    p = Lth("p","sum","M")
+    i1 = Add("sum","sum","N")
+    i2 = Add("count","count","one")
+    i3 = Add("answer","count","zero")
+    i0 = Bt("p",i1,i3)
+
+    p.add_next(i0)
+    i1.add_next(i2)
+    i2.add_next(p)
+    interp(p,env)
+    return env.get("answer")
+print(test_div(1, 3))
 
 def test_fact(n):
     """
@@ -88,5 +113,17 @@ def test_fact(n):
         >>> test_fact(3)
         6
     """
-    # TODO: Implement this method
+    env = Env({"N" : n, "res" : 1, "count": 1,"one" : 1,"zero":0})
+
+    p = Geq("p","N","count")
+    i1 = Mul("res","res","count")
+    i2 = Add("count","count","one")
+    i3 = Add("answer","res","zero")
+    i0 = Bt("p",i1,i3)
+
+    p.add_next(i0)
+    i1.add_next(i2)
+    i2.add_next(p)
+    interp(p,env)
     return env.get("answer")
+print(test_fact(-1))
